c902417d4fc7db7f75b14463c73465a44351e8e6
SylvaticGhost <143171907+SylvaticGhost@users.noreply.github.com>
2024-01-04 20:38:34 +0200

    LabWork8

---
+++ LabWork8/LabWork8.1/LabWork8.1.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string text;
+
+bool CheckIfCharIsPunctuation(char c) {
+    if ((c >= 33 && c <= 47) || (c >= 58 && c <= 64) || (c >= 92 && c <= 96) || (c >= 123 && c <= 126))
+        //ranges of UTF-8 punctations code
+        return true;
+    else return false;
+}
+
+int LenghtOfString(string s) {
+    int i = 0;
+    for (char c : s) {
+        i++;
+    }
+    return i;
+}
+
+void DeleteCharacter()
+{
+    for (int i = 1; i < LenghtOfString(text); i++) {
+        if ((text[i] == ' ' && text[i - 1] == ' ') || (
+            CheckIfCharIsPunctuation(text[i - 1]) && CheckIfCharIsPunctuation(text[i])))
+        {
+            text[i] = 127; // 127 UTF-8 code to delete
+            i--;
+        }
+    }
+}
+
+string FindLongestWord(string s) {
+    string max = "";
+    string current = "";
+    for (char c : s) {
+        if (c == ' ' || c == 127 || CheckIfCharIsPunctuation(c)) {
+            if (max.length() < current.length())
+                max = current;
+            current = "";
+        }
+        else
+            current += c;
+    }
+    if (max.length() < current.length()) {
+        max = current;
+        current = "";
+    }
+    return max;
+}
+
+int main() {
+    std::cout << "Введіть текстовий рядок" << std::endl;
+    getline(cin, text);
+
+    DeleteCharacter();
+
+    cout << "Відкорегований рядок: " << text << "\n";
+    cout << "Найдовше слово: " << FindLongestWord(text) << "\n";
+
+    return 0;
+}
+//Жовтий,, синій заєць бігали по лісу і волали:: "Е!!"..
---
+++ LabWork8/LabWork8.2.2/LabWork8.2.2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string text1, text2;
+
+int main() {
+
+    cout << "Введіть рядок ";
+    getline(cin, text1);
+    cout << "Введений перший рядок:" << text1 << "\n";
+
+    cout << "Введіть другий рядок рядок ";
+    getline(cin, text2);
+    cout << "Введений другий рядок:" << text2 << "\n";
+
+    cout << "довжина першого рядка " << text1.length() << "\n"; //.length()
+
+    cout << "порівняння двох рядків" << text1.compare(text2) << "\n"; //compare()
+
+    cout << "Додавання '!' до першого рядка\n";
+    text1.push_back('!');                               //push_back()
+    cout << "результат:" << text1 << "\n";
+
+    cout << "Видалення першого символу\n";
+    text1.erase(1, 1);
+
+    cout << "результат:" << text1 << "\n"; //erase()
+
+    cout << "Злиття рядків\n";
+
+    text1.append(text2);
+
+    cout << "результат:" << text1 << "\n";
+
+
+    return 0;
+}
+
